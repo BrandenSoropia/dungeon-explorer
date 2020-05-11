0b8a06d49c3404547e2c6b832b4600145f2ce3c6
BrandenSoropia <b.soropia@gmail.com>
2020-05-11 16:52:13 -0400

    Door opens via switch. Created single toggle switch prefab (reused switch prefab). Created door game object.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+  public bool isOpen = false;
+  public Sprite closedSprite;
+  public Sprite openSprite;
+
+  void Start()
+  {
+    GetComponent<SpriteRenderer>().sprite = isOpen ? openSprite : closedSprite;
+  }
+
+  public void ToggleState()
+  {
+    isOpen = !isOpen;
+    GetComponent<SpriteRenderer>().sprite = isOpen ? openSprite : closedSprite;
+    GetComponent<Collider2D>().enabled = !isOpen;
+  }
+
+}
